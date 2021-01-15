a6a4603cddcc929e56c4aaaa5f8529e374fe3f86
Matheus Mendes <matheus.unilog@gmail.com>
2021-01-15 20:21:52 -0300

    Cricle Test v3

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,8 +25,7 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
+  // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof(circle(10)), 'object');
     // Teste se o objeto retornado tem 3 entradas.
