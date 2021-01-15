35e36ec47c6f7b309c109200ba7937ea9b3e3ee8
ViniciusChagas <viniciushrcs@gmail.com>
2021-01-15 19:06:11 -0300

    corrigindo code climate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,18 +19,10 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 const calculator = {
-  add: (num1, num2) => {
-  return num1 + num2;
-  },
-  mult: (num1, num2) => {
-  return num1 * num2;
-  },
-  div: (num1, num2) => {
-  return Math.floor(num1 / num2);
-  },
-  sub: (num1, num2) => {
-  return num1 - num2;
-  },
+  add: (num1, num2) =>  num1 + num2;,
+  mult: (num1, num2) => num1 * num2;,
+  div: (num1, num2) => Math.floor(num1 / num2);,
+  sub: (num1, num2) => num1 - num2;,
 };
 
 module.exports = calculator;
