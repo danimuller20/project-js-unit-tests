597967b84b2631658a269fa00d156cfca79a81e4
Henrique-Moura <henrique_hgm1996@hotmail.com>
2021-01-15 17:11:17 -0300

    Requisito 1

--- src/average.js
+++ src/average.js
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-
+const average = (array) => {
+  let divisor = 0;
+  let contador = 0;
+  if (array.length === 0){
+    return undefined
+  } else {
+    for (let index = 0; index < array.length; index += 1) {
+      if (typeof array[index] !== 'number') {
+       return undefined
+      }
+      contador += array[index];
+    }
+    divisor = array.length
+  }
+  return Math.round(contador / divisor)
+};
 module.exports = average;
