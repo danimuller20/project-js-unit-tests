ac89953bac314378f109f096138ca42532233fb5
Osvaldo Borges <netoo70@gmail.com>
2021-01-15 15:54:10 -0300

    Requisito vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,17 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (nome !== undefined) {
+  let result = `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+  return result;
+  } else {
+    return undefined
+  }
+};
 
+console.log(vqv());
 module.exports = vqv;
