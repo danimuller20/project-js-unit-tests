e24f846530b7e3d4bc55c5ceb84053b92acf6806
Felipe Cardoso <felipedclc@gmail.com>
2021-01-15 17:50:46 -0300

    VQV

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,10 +25,13 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.strictEqual(typeof circle, 'object');
+
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(circle(1), {radius: 1, area: 3.14, circumference: 6.28});
+
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
