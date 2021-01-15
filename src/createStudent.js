3638bfc0ef2ba03d9890d4d872c73f336350bf90
Matheus Mendes <matheus.unilog@gmail.com>
2021-01-15 18:46:08 -0300

    Request Two, CodeClimate Corrections

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,8 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const createStudent = (nome) => {const student = {
+const createStudent = (nome) => {
+  const student = {
   name: nome,
 
   feedback: () => 'Eita pessoa boa!',
