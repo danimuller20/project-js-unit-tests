a47072e5ebe4af2bbf144eb061a4ccfb35158c30
Osvaldo Borges <netoo70@gmail.com>
2021-01-15 16:44:26 -0300

    Requisito createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,14 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (nome) => {
+  const estudante = {};
+  const resultName = `${nome}`;
+  const feedback = () => 'Eita pessoa boa!';
+  estudante.name = resultName;
+  estudante.feedback = feedback;
+
+  return estudante;
+};
 
 module.exports = createStudent;
