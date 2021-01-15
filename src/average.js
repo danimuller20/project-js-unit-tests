f627a422b6b02f8f05111eec988d9526509d2629
gabriel mendonca <gabriel.dev.lop@gmail.com>
2021-01-15 15:41:41 -0300

    Fixing code climate issues

--- src/average.js
+++ src/average.js
@@ -14,12 +14,12 @@
 
 const average = (arr) => {
   if (arr.length === 0 || arr === undefined) {
-    return;
+    return undefined;
   }
   let total = 0;
   for (let index = 0; index < arr.length; index += 1) {
     if (typeof arr[index] !== 'number') {
-      return;
+      return undefined;
     }
     total += arr[index];
   }
