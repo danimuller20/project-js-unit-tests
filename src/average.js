7d9ee89a93a1f94b5fd68c2e81334aa841e2a200
MarcosAlbuquerque <mpdsa20@gmail.com>
2021-01-15 17:21:09 -0300

    Requisito 1 - enviando para validação

--- src/average.js
+++ src/average.js
@@ -12,8 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // add your implementation here
+const average = (arr) => {
+  let soma = 0;
+  let media = arr.length;
+
+  if (typeof(arr) === 'object' && arr.length === 0) return undefined
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof(arr[i]) !== 'number') return undefined
+
+    soma += arr[i]
+  }
+
+  return Math.round(soma / media);
 };
 
 module.exports = average;
