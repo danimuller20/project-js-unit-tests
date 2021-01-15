31ccf3ebc6064073b342934e736decbf78ffcd94
Luciano_Lodi <lrlodi@gmail.com>
2021-01-15 19:01:23 -0300

    CC

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,6 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (string) => { name: string, feedback: () => 'Eita pessoa boa!',};
+const createStudent = (string) => name: string, feedback: () => 'Eita pessoa boa!',;
 
 module.exports = createStudent;
