a5ad534475261d1df01de0fe16f8247c4aafb960
Osvaldo Borges <netoo70@gmail.com>
2021-01-15 15:33:36 -0300

    Requisito Average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,24 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let sum = 0;
+  let result = 0;
+  if (array.length == 0) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof(array[index]) !== 'string') {
+      sum += array[index];
+    }else {
+      return undefined;
+    }
+  }
+  result = (sum/array.length);
+  return Math.round(result);
+};
+
+const array = [];
+console.log(average(array));
 
 module.exports = average;
