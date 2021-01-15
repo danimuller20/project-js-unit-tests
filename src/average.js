1a45f5274743ca3ff76500191a89d62acbb13498
Felipe Cardoso <felipedclc@gmail.com>
2021-01-15 15:31:17 -0300

    code climate

--- src/average.js
+++ src/average.js
@@ -13,11 +13,9 @@
 */
 
 const average = (arr) => {
-  const newArr = [];
   let soma = 0;
-  let media = 0;
   for (let i = 0; i < arr.length; i += 1) {
-    soma = soma + arr[i];
+    soma += arr[i];
   }
   return Math.ceil(soma / arr.length);
 };
