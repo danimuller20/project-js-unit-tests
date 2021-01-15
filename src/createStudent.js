3fd74d1194961cd88b55a3e7b078439dfe8c1654
MarcioFujita <hanker78@gmail.com>
2021-01-15 18:33:54 -0300

    createStudent V2

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,10 +18,9 @@
 const createStudent = (nome) => {
   const student = {
     name: nome,
-    feedback: () => 'Eita pessoa boa!'
-  }
+    feedback: () => 'Eita pessoa boa!',
+  };
   return student;
-
 };
 
 module.exports = createStudent;
