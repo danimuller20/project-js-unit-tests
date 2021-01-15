0d6eed06129102b08d97877bae81533f36756130
Diogenes <diogenesfelipe.s@outlook.com>
2021-01-15 20:01:26 -0300

    Refatorando o productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -1,8 +1,8 @@
 /* eslint-disable max-len*/
 /* eslint-disable no-unused-vars */
 
-const assert = require('assert');
-const productDetails = require('../src/productDetails');
+const assert = require("assert");
+const productDetails = require("../src/productDetails");
 
 /*
   Dadas duas strings que representam nomes de produtos, retorne um array contendo dois objetos com os detalhes dos respectivos produtos.
@@ -31,20 +31,29 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#productDetails', () => {
-  it('tests the function has the correct behaviour', () => {
+describe("#productDetails", () => {
+  it("tests the function has the correct behaviour", () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
-    assert.strictEqual(Array.isArray(productDetails('x', 'y')), true);
+    assert.strictEqual(Array.isArray(productDetails("x", "y")), true);
     // Teste que o array retornado pela função contém dois itens dentro.
-    assert.strictEqual(productDetails('x','y').length, 2);
+    assert.strictEqual(productDetails("x", "y").length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.strictEqual(typeof productDetails()[0], 'object');
-    assert.strictEqual(typeof productDetails()[1], 'object');
+    assert.strictEqual(typeof productDetails()[0], "object");
+    assert.strictEqual(typeof productDetails()[1], "object");
     // Teste que os dois objetos são diferentes entre si.
-    assert.notDeepStrictEqual(productDetails('x', 'y')[0],productDetails('x', 'y')[1]);
+    assert.notDeepStrictEqual(
+      productDetails("x", "y")[0],
+      productDetails("x", "y")[1]
+    );
     // (Difícil) Teste que os dois productIds terminam com 123.
-    assert.strictEqual(productDetails()[0].details.productId.slice(-3, productDetails()[0].details.productId.length), '123')
-    assert.strictEqual(productDetails()[1].details.productId.slice(-3, productDetails()[1].details.productId.length), '123')
+    assert.strictEqual(
+      productDetails()[0].details.productId.slice(-3, productDetails()[0].details.productId.length),
+      "123"
+    );
+    assert.strictEqual(
+      productDetails()[1].details.productId.slice(-3,productDetails()[1].details.productId.length),
+      "123"
+    );
   });
 });
