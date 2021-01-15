366002152e7982c9fde4ca3f4154ce95ba67627f
rafaelgeronimo <rafaelgeronimo@outlook.com>
2021-01-15 18:27:23 -0300

    Function createStudent()

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const newStudent = {
+    name: name,
+    feedback:  () => { return 'Eita pessoa boa!' },
+  };
+  return newStudent;
+};
 
 module.exports = createStudent;
