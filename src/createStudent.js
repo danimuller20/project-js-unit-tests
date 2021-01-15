e2b5311f90729bc0ce166b2c36541b094ba5edaf
danielfasanaro <danielfasanaro@gmail.com>
2021-01-15 18:40:14 -0300

    5th Requiriment - Implemented createStudent function

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const myStudent = {
+    name,
+    feedback: () => 'Eita pessoa boa!',
+  }
+  return myStudent;
+};
 
 module.exports = createStudent;
