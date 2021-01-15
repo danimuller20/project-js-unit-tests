8dff76c1a30a3cd9bdb63e2f85043485e32b8440
LeoMajevski <leonardo.majevski@gmail.com>
2021-01-15 20:54:52 -0300

    Implements unit tests of the productDetails() function

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    // assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productDetails()), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.entries(productDetails()).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.deepStrictEqual(typeof Object.entries(productDetails()), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.notDeepStrictEqual(Object.values(productDetails('paramA', 'paramB'))[0], Object.values(productDetails('paramA', 'paramB'))[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(productDetails('paramA', 'paramB')[0].details.productId.includes('123'), productDetails('paramA', 'paramB')[1].details.productId.includes('123'));
   });
 });
