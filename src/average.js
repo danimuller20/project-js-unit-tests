bba08f321cbdbebe74046b2d21e1c60b5d3fab5d
Giovani L D <giovani.diccini@hotmail.com>
2021-01-15 15:47:17 -0300

    first requirement codeclimate fix

--- src/average.js
+++ src/average.js
@@ -13,19 +13,17 @@
 */
 
 const average = (array) => {
-  let sum  = 0;
+  let sum = 0;
   if (array.length === 0) {
     return undefined;
-  } else {
-    for (let index = 0; index < array.length ; index += 1) {
-      if (typeof(array[index]) !== 'number') {
-        return undefined;
-      } else {
-        sum += array[index];
-      }
-    }
-    return Math.round(sum / array.length);
   }
-};
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
+    }
+    sum += array[index];    
+  }
+  return Math.round(sum / array.length);
+}
 
 module.exports = average;
