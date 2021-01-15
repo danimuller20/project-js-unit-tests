30715546d05ef0bb549df41bd1f01e18ff3639d3
ViniciusChagas <viniciushrcs@gmail.com>
2021-01-15 17:02:15 -0300

    corrigindo issues Code Climate 4

--- src/average.js
+++ src/average.js
@@ -16,7 +16,7 @@ const average = (array) => {
   let sumOfElements = 0;
   if (array.length === 0) { return undefined; }
   for (let i = 0; i < array.length; i += 1) {
-      if (typeof (array[i]) !== 'number') { return undefined; }
+    if (typeof (array[i]) !== 'number') { return undefined; }
     sumOfElements += array[i];
   }
   return Math.round((sumOfElements / array.length));
