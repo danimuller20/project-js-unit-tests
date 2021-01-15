8a02989f7c454d3065915bf81fff9b6bc54c3641
RodrigoNeoEx <draja.acc@gmail.com>
2021-01-15 20:25:09 -0300

    climate2

--- src/average.js
+++ src/average.js
@@ -14,16 +14,16 @@
 
 const average = (teste) => {
   let sum = 0;
-  if(teste.length === 0) {
+  if (teste.length === 0) {
     return undefined;
   }
-  for(let index = 0; index < teste.length; index += 1) {
-    if(typeof(teste[index]) !== 'number') {
+  for (let index = 0; index < teste.length; index += 1) {
+    if (typeof (teste[index]) !== 'number') {
       return undefined;
-    } 
+    }
     sum += teste[index];
   }
   return Math.round(sum / teste.length);
-}
+};
 
 module.exports = average;
