5f500e3ae80ffa284f11c3b66ed04a93f13fa1f8
Felipe Shcolnik <shcolnik@gmail.com.br>
2021-01-15 18:28:19 -0300

    codeclimate

--- src/average.js
+++ src/average.js
@@ -12,37 +12,35 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-
-const average = (array) => {
-  if (validateAll(array) === 1) {
-    let sum = 0;
-    for (index = 0; index < array.length; index +=1) {
-      sum += array[index];
-    }
-   return Math.round(sum/array.length);
-  } return undefined;
-}
-
 const validateEmpty = (array) => {
   if (array.length === 0) {
-   return undefined;
+    return undefined;
   } return 1;
-}
+};
 
-function validateAll(array) {
-  if (validateEmpty(array) === 1 && validateNumber(array) === 1) {
-  return 1;
-  } return undefined;
-}
 const validateNumber = (array) => {
   for (index = 0; index < array.length; index +=1) {
-    let posArray = array[index];
-    if (typeof(posArray) !== 'number') {
+    const posArray = array[index];
+    if (typeof (posArray) !== 'number') {
       return undefined;
     }
   }
   return 1;
+};
+function validateAll(array) {
+  if (validateEmpty(array) === 1 && validateNumber(array) === 1) {
+    return 1;
+  } return undefined;
 }
+const average = (array) => {
+  if (validateAll(array) === 1) {
+    let sum = 0;
+    for (index = 0; index < array.length; index += 1) {
+      sum += array[index];
+    }
+    return Math.round(sum / array.length);
+  } return undefined;
+};
 
 console.log(average([]));
 
