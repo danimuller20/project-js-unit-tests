99fc752a72910d9b938312069c77736c4a2effda
Christopher <chris.tryber@gmail.com>
2021-01-15 16:42:10 -0300

    Requisito um e dois V1.1

--- src/average.js
+++ src/average.js
@@ -14,16 +14,16 @@
 
 const average = (array) => {
   let sum = 0;
-  if (array.length === 0){
-    return undefined
+  if (array.length === 0) {
+    return undefined;
   }
-  for (let index = 0; index < array.length; index += 1){
-    if (typeof(array[index]) !== 'number'){
-      return undefined
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof (array[index]) !== 'number') {
+      return undefined;
     }
-    sum += array[index]
+    sum += array[index];
   }
-  return Math.round(sum / array.length)
+  return Math.round(sum / array.length);
 };
 
 module.exports = average;
