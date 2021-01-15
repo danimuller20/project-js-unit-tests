0336c402d734d33d416ca16416a1788eac876c6c
Matheus Mendes <matheus.unilog@gmail.com>
2021-01-15 18:44:29 -0300

    Request Two

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,11 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
+const createStudent = (nome) => {const student = {
+  name: nome,
 
-const createStudent = () => {};
+  feedback: () => 'Eita pessoa boa!',
+};
+return student;};
 
 module.exports = createStudent;
