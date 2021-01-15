72e57eefa4dae3cee9d9bb79160cb996ac94b989
Daniela Muller <danielamuller20@gmail.com>
2021-01-15 17:53:01 -0300

    requisito tres

--- src/vqv.js
+++ src/vqv.js
@@ -20,8 +20,8 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  let message = 'Oi, meu nome é '+ nome +'!\n' +
-  'Tenho ' + idade +' anos,\n' +
+  const message = 'Oi, meu nome é ' + nome + '!\n' +
+  'Tenho ' + idade + ' anos,\n' +
   'trabalho na Trybe e mando muito em programação!\n' +
   '#VQV!';
   return message;
