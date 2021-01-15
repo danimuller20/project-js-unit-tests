9f5af67fee891a9e43097c39975bc4cb23493a58
Natalia Negreiros <nataliaknegres@gmail.com>
2021-01-15 19:29:24 -0300

    add test productDetails.js / all tests passed

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,7 +28,6 @@ describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    // assert.strictEqual(typeof circle, 'function');
     assert.strictEqual(typeof circle(5), 'object');
     // Teste se o objeto retornado tem 3 entradas.
     assert.strictEqual(Object.entries(circle(5)).length, 3);
