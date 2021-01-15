f59380c3d92822eccbc30c448bfc2355c1925509
ggaldino95 <ggaldino95@gmail.com>
2021-01-15 16:38:31 -0300

    Small codeclimate fix

--- src/vqv.js
+++ src/vqv.js
@@ -17,14 +17,14 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome == undefined || idade == undefined) {
-    return undefined
-  } else {
-    return `Oi, meu nome é ${nome}!
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
-  }
 };
 
 module.exports = vqv;
