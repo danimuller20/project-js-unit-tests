8fcb8082a10f898e636f472fc3cfd9b835548540
brenno-calado <brenno.calado@gmail.com>
2021-01-15 19:18:05 -0300

    js unit test circle.spec.js

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,7 +25,13 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
+    assert.strictEqual(typeof circle(3), 'object');
+    assert.strictEqual(Object.keys(circle(3)).length, 3);
+    assert.strictEqual(circle(), undefined);
+    assert.strictEqual(parseFloat((Object.values(circle(2))[2]).toPrecision(2)), 13);
+    assert.strictEqual(parseFloat((Object.values(circle(3))[1]).toPrecision(2)), 28);
+    assert.deepStrictEqual(Object.values(circle(3)), 
+      [3, 28.259999999999998, 18.84]);
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
     // Teste se o objeto retornado tem 3 entradas.
