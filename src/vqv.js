0364bde5b9b80a4488e7d38168f58a788cc7a188
marcusWittho <witthoeft.mcs@hotmail.com>
2021-01-15 17:14:19 -0300

    Resolução do VQV!

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const text =
+  `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+
+  return text;
+};
+
+console.log(vqv('Tunico', 29));
 
 module.exports = vqv;
