a2c9700efbb0737fb5cbe56fda448823f1e6ad23
Isaac Batista <isaacbatst@gmail.com>
2021-01-15 19:14:10 -0300

    Update restaurant.spec.js

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -54,7 +54,8 @@ describe('#createMenu', () => {
     assert.fail();
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
     // ```
-    // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
+    // createMenu(objetoQualquer) // Retorno: { fetchMenu: () => objetoQualquer }  
+    // `fetchMenu` deve ser uma função quer retorna o `objetoQualquer`
     // ```
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
