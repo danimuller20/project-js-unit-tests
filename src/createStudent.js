17fd8b455c3ab28a896b28b97349445c1ad67dce
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-01-16 18:30:03 -0300

    Change name and nome position on createStudent.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,9 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
+const createStudent = (nome) => {
   const student = {
-    nome: name,
+    name: nome,
     feedback: () => 'Eita pessoa boa!',
   };
   return student;
