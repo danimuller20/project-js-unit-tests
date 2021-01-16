46694c9309db13d76d9d50171f51b14d946b96de
Bernardo Smith <bernardostech@gmail.com>
2021-01-16 14:52:02 -0500

    questao 3 de primeira, gracas a deus

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (param) => {
+  const estudante = { name: param ,
+  feedback: ()=>{
+    return 'Eita pessoa boa!'
+  }
+  }
+  return estudante;
+};
 
 module.exports = createStudent;
