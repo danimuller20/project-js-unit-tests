ee0452f326f8312c1634d84d64bea4ca19334df2
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-01-16 01:34:13 -0300

    Unexpected parentheses around single function argument having a body with no curly braces

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,7 +15,7 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (nome) => ({ feedback: () => 'Eita pessoa boa!', name: nome,});
+const createStudent = nome => ({ feedback: () => 'Eita pessoa boa!', name: nome,});
 
 // const assert = require('assert');
 // assert.strictEqual(typeof(createStudent()), 'object');
