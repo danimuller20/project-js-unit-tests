a269194c24cb9437f3688223463014272e8876ff
IvanRafael-Dev <ivanrafael.dev@gmail.com>
2021-01-16 01:19:35 -0300

    Funcao Circle implementada

--- src/circle.js
+++ src/circle.js
@@ -18,14 +18,41 @@
         Use esse conhecimento para te ajudar a lidar com possíveis problemas que esses testes trarão!
 */
 
-const circle = (radius) => {
+const circle = radius => {
   const PI = 3.14;
-  if (!radius) { return undefined; }
+  if (!radius) {
+    return undefined;
+  }
   return {
     radius,
     area: PI * radius * radius,
     circumference: 2 * PI * radius,
   };
 };
+
+const assert = require('assert');
+
+// assert.strictEqual(parseFloat(circle(3).area.toPrecision(4)), 28.26)
+console.log(Object.values(circle(3)));
+
+// Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+// assert.deepStrictEqual(circle(3).radius, 3);
+// Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+// assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84});
+
+assert.deepStrictEqual(circle(3), { radius: 3, area: 28.259999999999998, circumference: 18.84 });
+// console.table(circle(3))
+// assert.strictEqual(circle(2).circumference, 12.56)
+
+// assert.strictEqual(Object.keys(circle(2)).length, 3);
+
+// console.log(typeof(circle(2)))
+// console.log(Object.keys(teste).length)
+// console.log(typeof(circle()))
+// console.table(circle(2))
+
+// assert.strictEqual(Object.keys(circle(2)).length, 3)
+// Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+// assert.strictEqual(circle(), undefined)
 
 module.exports = circle;
