0bc4036790c48d5a7897653644adc6f3ccb9397f
Micaela Alves <micaela.alves93@gmail.com>
2021-01-16 04:17:25 -0300

    Ajustes code climate

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,7 +15,7 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = nameStudent => {
+const createStudent = (nameStudent) => {
   return {
     name: nameStudent,
     feedback: () => 'Eita pessoa boa!',
