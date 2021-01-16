d37367c7b1668d360a79fe7171155c295f956fb4
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-01-16 17:59:15 -0300

    requisito 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,9 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
+const createStudent = (nome) => {
   const object = {
-    name: name,
+    name: nome,
     feedback: () => 'Eita pessoa boa!',
   };
   return object;
