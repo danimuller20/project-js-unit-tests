2e3d3e4aeb202b0d4b5ec15a2d7cf95a0c7ee974
Tiago de Almeida Paz <tiagopaz.dev@gmail.com>
2021-01-16 14:51:03 -0300

    refatorando requisito 1

--- src/average.js
+++ src/average.js
@@ -14,10 +14,10 @@
 
 const average = (array) => {
   let sum = 0;
+  if (array.length === 0) {
+    return undefined;
+  }
   for (let index = 0; index < array.length; index += 1) {
-    if (array.length === 0 || typeof array[index] !== 'number') {
-      return undefined;
-    }
     sum += array[index];
   }
   return Math.round(sum / array.length);
