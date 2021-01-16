99c7213a330f0e13d2557d4d685412ae4efc3681
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-01-15 23:25:54 -0300

    codeclime10

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,9 @@
 const createStudent = (nome) => {
   return {
     name: nome,
-    feedback: () => ('Eita pessoa boa!'),
+    feedback: function() {
+      return ('Eita pessoa boa!');
+    }
   };
 };
 
