adcd07fdaf56be6bae13d1ae1695c7b415173d55
Cristiano Lopes Duarte <tkdcris@gmail.com>
2021-01-15 23:37:00 -0300

    codeclime12

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,11 +15,11 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-function createStudent (nome) {
+function createStudent(nome) {
   return {
     name: nome,
     feedback: () => 'Eita pessoa boa!',
   };
-};
+}
 
 module.exports = createStudent;
