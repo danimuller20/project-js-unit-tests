f8c47e055d65a525b95f52aa395a0ea536fa8712
Pedro Henrique <pedroenrique.400@gmail.com>
2021-01-16 12:57:17 -0300

    faz a os testes para a função productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -30,15 +30,22 @@ const productDetails = require('../src/productDetails');
 
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
-
+  const productdetails = productDetails('coca','mate');
+  const checkIfthereIs123Item1 = productdetails[0].details.productId.indexOf('123') > -1;
+  const checkIfthereIs123Item2 = productdetails[1].details.productId.indexOf('123') > -1;
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    assert.strictEqual(Array.isArray(productdetails), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productdetails.length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof (productdetails[0]) && typeof(productdetails[1]), 'object');
     // Teste que os dois objetos são diferentes entre si.
+    assert.strictEqual(productdetails[0].name === productdetails[1].name, false);
     // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(checkIfthereIs123Item1 && checkIfthereIs123Item2, true);
+
   });
 });
