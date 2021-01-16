0d4ef79e02562d03358047b8c4378e7165540880
Pamela Anderson Sales <pamelasalesr@gmail.com>
2021-01-16 16:04:58 -0300

    Fix Trailing spaces not allowed issue

--- src/average.js
+++ src/average.js
@@ -32,7 +32,7 @@ const average = (array) => {
   let averageArray = 0;
   if (array.length === 0) return undefined;
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof (array[index]) !== 'number') return undefined;    
+    if (typeof (array[index]) !== 'number') return undefined;
     averageArray = array.reduce((a, b) => (a + b)) / array.length;
   }
   return Math.round(averageArray);
