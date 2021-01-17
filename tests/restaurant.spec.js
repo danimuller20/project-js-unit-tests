679dcf172c5bb76e047c43d576765dc66dbdc18c
saulokirchmaier <saulokirchmaier@gmail.com>
2021-01-17 14:27:15 -0300

    createMenu ex. 5 - test

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -80,7 +80,7 @@ describe('#createMenu', () => {
     // const objetoRetornado = createMenu(objetoQualquer);
     // objetoRetornado.consumption // Retorno: []
     // ```
-    assert.strictEqual(createMenu().consumption, []);
+    assert.strictEqual(createMenu({}).consumption, []);
     // Agora faça o PASSO 2 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
     // TESTE 5: Verifique que chamar uma função associada à chave `ord(er` no objeto retornado, passando uma string como parâmetro, como `objetoRetornado.order('coxinha')`, tal string é adicionada ao array retornado em `objetoRetornado.consumption
