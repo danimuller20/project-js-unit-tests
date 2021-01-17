7974d9dd70e63e03e40ff58f085dac44ff2d2a07
Diego Vini <analistamm@gmail.com>
2021-01-17 17:23:26 -0300

    Requisito 07 mycounter ajustando cc

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter_01 = 2; counter_01 <= 3; counter_01 += 1) {
-      myArray.push(counter_01);
+    for (let secondCounter = 2; secondCounter <= 3; secondCounter += 1) {
+      myArray.push(secondCounter);
     }
   }
   return myArray;
