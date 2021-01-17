bfcc7f87fee4bdcf0e1bf2a7c1e6ca859f33eacd
Mathaus <mathausannibal@gmail.com>
2021-01-17 16:56:42 -0300

    adiciona productsDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,15 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
+    const details = productDetails('Alcool gel', 'Máscara')
+    //assert.strictEqual(typeof details, 'array')
     // ESCREVA SEUS TESTES ABAIXO:
+    assert.strictEqual(details.length, 2)
+    assert.strictEqual(typeof details[0], 'object')
+    assert.strictEqual(typeof details[1], 'object')
+    assert.notStrictEqual(details[0], details[1])
+    assert.strictEqual(details[0].details.productId.substr(-3), '123')
+    assert.strictEqual(details[1].details.productId.substr(-3), '123')
     // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
