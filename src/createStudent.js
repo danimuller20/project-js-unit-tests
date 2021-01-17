f56cd70eed29b5ca7e53bc62ef56e7d1399bd5d4
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-17 15:16:17 -0300

    code climate

--- src/createStudent.js
+++ src/createStudent.js
@@ -18,7 +18,9 @@
 const createStudent = (nome) => {
   return {
     name: nome,
-    feedback: () => { return 'Eita pessoa boa!' },
+    feedback: function () {
+      return 'Eita pessoa boa!'
+    }
   };
 };
 
