3f4db74ad5be44fd5530328dc4abc1f1f2384901
Khyrki <khyrkimitsunobi@gmail.com>
2021-01-17 16:32:25 -0300

    code clumate

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,8 +17,8 @@
 
 const myCounter = () => {
   var myArray = [];
-  for (let counter = 0; counter <= 3; counter += 1) {
-    myArray.push(counter);
+  for (let counter0 = 0; counter0 <= 3; counter0 += 1) {
+    myArray.push(counter0);
     for (let counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
     }
@@ -27,5 +27,3 @@ const myCounter = () => {
 };
 
 module.exports = myCounter;
-
-console.log(myCounter())
