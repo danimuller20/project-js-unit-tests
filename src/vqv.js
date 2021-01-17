e60481857c122df2742049008d5d26d75bd1e349
LucioOSilva <lucioosilva00@gmail.com>
2021-01-17 11:56:03 -0300

    Req8- added function vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,12 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  let phrase = undefined;
+  if (!(nome === undefined) || !(idade === undefined)) {
+    phrase = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  }
+  return phrase;
+}
 
 module.exports = vqv;
