015aab7ee74e352e2e82f78d7796f6243feee6bf
edison-ps <edison@osite.com.br>
2021-01-17 14:31:06 -0300

    createStudent V1.08

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,13 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
-  return {
+const createStudent = (name) => ({
     name,
-    feedback: function feedback() {
-      return 'Eita pessoa boa!';
-    },
-  };
-};
+    feedback: () => 'Eita pessoa boa!',
+});
 
 module.exports = createStudent;
