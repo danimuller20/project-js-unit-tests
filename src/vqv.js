9f38ad6e93993a99124ed19d95cb4f9c62d1736b
edison-ps <edison@osite.com.br>
2021-01-17 16:52:49 -0300

    vqv V1.01

--- src/vqv.js
+++ src/vqv.js
@@ -17,13 +17,13 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome == undefined && idade == undefined) {
-    return undefined
+  if (nome === undefined && idade === undefined) {
+    return undefined;
   }
   return `Oi, meu nome é ${nome}!\n` +
   `Tenho ${idade} anos,\n` +
-  `trabalho na Trybe e mando muito em programação!\n` +
-  `#VQV!`;
+  'trabalho na Trybe e mando muito em programação!\n' +
+  '#VQV!';
 };
 
 module.exports = vqv;
