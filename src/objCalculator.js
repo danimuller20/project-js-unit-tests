7696f21c0e9cb997f1165b6841d89e5208cb1765
Diego Vini <analistamm@gmail.com>
2021-01-17 16:56:08 -0300

    Requisito 06 object calculator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,6 +19,23 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = {};
-
+const calculator = {
+  add: (num1, num2) => {
+    const soma = num1 + num2;
+    return Math.trunc(soma);
+  },
+  mult:(num1, num2) => {
+    const multiplicacao = num1 * num2;
+    return Math.round(multiplicacao);
+  },
+  div: (num1, num2) => {
+    const divisao = num1 / num2;
+    return Math.trunc(divisao);
+  },
+  sub: (num1, num2) => {
+    const subtracao = num1 - num2;
+    return Math.trunc(subtracao);
+  }
+};
+console.log(calculator.add(1,3))
 module.exports = calculator;
