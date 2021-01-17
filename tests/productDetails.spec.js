3d08dba4c3291205b097885c91e25bff28b6a414
Gabriel Castro <kastro.gabriel17@gmail.com>
2021-01-17 18:11:53 -0400

    ajustando test productDetails

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -38,7 +38,7 @@ describe('#productDetails', () => {
     // Teste que o array retornado pela função contém dois itens dentro.
     assert.strictEqual(Object.entries(productDetails()).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
-    assert.deepStrictEqual(typeof Object.entries(productDetails()), 'object');
+    assert.deepStrictEqual(typeof Object.keys(productDetails('a', 'b')), 'object');
     // Teste que os dois objetos são diferentes entre si.
     assert.notDeepStrictEqual(Object.values(productDetails('a', 'b'))[0], Object.values(productDetails('a', 'b'))[1]);
     // (Difícil) Teste que os dois productIds terminam com 123.
