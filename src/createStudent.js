f438f6fa284a0650132dadf96c8493c37e2720f0
Marcos Albuquerque <mpdsa20@gmail.com>
2021-01-17 16:58:13 -0300

    reduzido linhas da funcao

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,13 +14,9 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const createStudent = (name) => {
-  return {
-    name: name,
-    feedback: () => {
-	    return 'Eita pessoa boa!';
-    },
-  };
-};
+const createStudent = (nome) => ({
+  name: nome,
+  feedback: () => 'Eita pessoa boa!';
+});
 
 module.exports = createStudent;
