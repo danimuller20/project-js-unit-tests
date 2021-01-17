8ad189d91b710ecd478ea9916a054cce742dd240
Renato Feitosa <pythaqua@gmail.com>
2021-01-16 21:20:42 -0300

    [feat](restaurant.js) add fourth step to the 9th requirement

--- src/restaurant.js
+++ src/restaurant.js
@@ -102,7 +102,8 @@ const createMenu = menu => ({
       const productPrice = getProductPrice(product, menu);
       bill += productPrice;
     }
-    return bill;
+    bill *= 1.1
+    return parseFloat(bill.toPrecision(4));
   },
 });
 
