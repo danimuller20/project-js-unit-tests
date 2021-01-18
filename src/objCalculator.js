3a61412552f1b2ed50572bd7de1ce91a88d07946
Pablo Iago <evangelista.pabloiago@gmail.com>
2021-01-18 15:14:57 -0300

    Requisito 6 pronto codeclimate espaços

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -19,8 +19,8 @@
   calculator.div(3, 2) // Retorno: 1;
 */
 
-const calculator = 
-({  
+const calculator =
+({
   add: (number1, number2) => number1 + number2,
   mult: (number1, number2) => number1 * number2,
   div: (number1, number2) => Math.floor(number1 / number2),
