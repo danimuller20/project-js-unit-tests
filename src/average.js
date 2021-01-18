a10d9484e729cb1c3b245b7c9f4632f2f4344d78
cleber-teixeira <cleberlt@gmail.com>
2021-01-18 13:44:07 -0300

    Requisito 01 - Função "average()": O programa retorna a média dos valores. :white_check_mark:

--- src/average.js
+++ src/average.js
@@ -1,8 +1,8 @@
 /*
-  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos.
+  A função average recebe um array (tamanho variável) e retorna a média dos valores recebidos. [x]
   Caso a função receba algum valor não númerico ou um array vazio,
-  o valor undefined deve ser retornado.
-  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1.
+  o valor undefined deve ser retornado. [x]
+  Todos os resultados devem ser arredondados para valores inteiros. Ex: 4,6 vira 5; 1,3 vira 1. [x]
 
   Parâmetros:
     - Um array. Exemplos: [1, 2]; [1, 2, 3, 4, 5]; [1, 2, '3']; [];
@@ -12,6 +12,20 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let media;
+  let sun = 0;
+  if (array === [] || array === 0 || array === undefined || array === NaN) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof(array[index]) === "string") {
+      return undefined;
+    }
+    sun += array[index];
+  }
+  media = Math.round(parseInt(sun) / parseInt(array.length));
+  return media;
+}
 
 module.exports = average;
