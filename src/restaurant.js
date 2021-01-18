c9bedfdf5b650d5da54ab952ac938a6d4a8e8677
marcusWittho <witthoeft.mcs@hotmail.com>
2021-01-18 12:04:32 -0300

    Resolução do createMenu

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,44 +71,78 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = (string) => {
-  const obj = {
-    fetchMenu: string,
+// Tentativa 3
+const createMenu = (obj) => {
+  const mainObj = {
+    fetchMenu: obj,
     comsuption: [],
-    order: order1 => obj.comsuption.push(order1),
+    order: order1 => mainObj.comsuption.push(order1),
+    pay: () => payment(mainObj.fetchMenu, mainObj.comsuption),
+  };
+  return mainObj;
+};
+
+function payment(fetch, comsuption) {
+  let count = 0;
+  for (let index = 0; index < comsuption.length; index += 1) {
+    if (comsuption[index] in fetch.food) {
+      count += fetch.food[comsuption[index]];
+    } else if (comsuption[index] in fetch.drink) {
+      count += fetch.drink[comsuption[index]];
+    }
+  };
+  return count;
+}
+
+module.exports = createMenu;
+
+/*
+// Tentativa 1 (Funciona)
+const createMenu = (obj) => {
+  const mainObj = {
+    fetchMenu: obj,
+    comsuption: [],
+    order: order1 => mainObj.comsuption.push(order1),
     pay: () => {
       let count = 0;
-      const fetch = obj.fetchMenu;
-      for (let index = 0; index < obj.comsuption.length; index += 1) {
-        if (obj.comsuption[index] in fetch.food) {
-          count += fetch.food[obj.comsuption[index]];
+      const fetch = mainObj.fetchMenu;
+      for (let item of mainObj.comsuption) {
+        if (fetch.food.hasOwnProperty(item)) {
+          count += fetch.food[item];
         }
       }
-      for (let index = 0; index < obj.comsuption.length; index += 1) {
-        if (obj.comsuption[index] in fetch.drink) {
-          count += fetch.drink[obj.comsuption[index]];
+      for (let item of mainObj.comsuption) {
+        if (fetch.drink.hasOwnProperty(item)) {
+          count += fetch.drink[item];
         }
       }
       return count;
     }
-  }
-  return obj;
+  };
+  return mainObj;
 };
-/*
-pay: () => {
-  let count = 0;
-  const fetch = obj.fetchMenu;
-  for (let item of obj.comsuption) {
-    if (fetch.food.hasOwnProperty(item)) {
-      count += fetch.food[item];
-    }
-  }
-  for (let item of obj.comsuption) {
-    if (fetch.drink.hasOwnProperty(item)) {
-      count += fetch.drink[item];
-    }
-  }
-  return count;
-}
 */
-module.exports = createMenu;
+
+/*
+// Tentativa 2  (Funciona)
+const createMenu = (obj) => {
+  const mainObj = {
+    fetchMenu: obj,
+    comsuption: [],
+    order: order1 => mainObj.comsuption.push(order1),
+    pay: () => {
+      let count = 0;
+      const fetch = mainObj.fetchMenu;
+      for (let index = 0; index < mainObj.comsuption.length; index += 1) {
+        if (mainObj.comsuption[index] in fetch.food) {
+          count += fetch.food[mainObj.comsuption[index]];
+        } else if (mainObj.comsuption[index] in fetch.drink) {
+          count += fetch.drink[mainObj.comsuption[index]];
+        }
+      }
+      return count;
+    }
+  };
+  return mainObj;
+};
+*/
