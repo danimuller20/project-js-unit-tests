8d45f1b08138677fb43a297f57d249e76f665e85
Lucas Silva <martssilvapro@gmail.com>
2021-01-18 11:45:43 -0300

    agora vai

--- src/vqv.js
+++ src/vqv.js
@@ -16,8 +16,6 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => { 
-  return `Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação! #VQV`; 
-};
+const vqv = (nome, idade) => { `Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação! #VQV`; };
 
 module.exports = vqv;
