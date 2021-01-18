2454f0f9c8f575e53e1265c0be4e849c40b456d7
alailsonko <aalailson3@gmail.com>
2021-01-18 00:51:07 -0300

    tests createStudent

--- src/createStudent.js
+++ src/createStudent.js
@@ -14,7 +14,7 @@
     estudante.name // Retorna: 'Leandrão, o Lobo Solitário'
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
-const createStudent = (name) => ({
+const createStudent = name => ({
   name,
   feedback: () => 'Eita pessoa boa!',
 });
