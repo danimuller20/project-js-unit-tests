822457f374a048d0ad655ae37cb2a49955aed41e
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-01-18 12:30:09 -0300

    funcao myCounter

--- src/myCounter.js
+++ src/myCounter.js
@@ -19,8 +19,8 @@ const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (var scndcounter = 2; scndcounter <= 3; scndcounter += 1) {
+      myArray.push(scndcounter);
     }
   }
   return myArray;
