4e5fe6a3f5bd9b437a52de3f83e5c10983f922a6
Poeta <pedrotrybe@gmail.com>
2021-01-18 15:18:58 -0300

    code Climate

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,8 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome && idade) {return `Oi, meu nome é ${nome}!
+  if (nome && idade) {
+    return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
