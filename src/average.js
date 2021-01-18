5f1300d7350964374ccf093c1e2268c61b5a4b53
Rafa Reis <rafaelrnascimento2006@gmail.com>
2021-01-18 16:08:26 -0300

    organizando codigo

--- src/average.js
+++ src/average.js
@@ -14,13 +14,9 @@
 
 const average = (array) => {
   let sum = 0;
-  if (array.length === 0) {
-    return undefined;
-  }
+  if (array.length === 0) { return undefined; }
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof array[index] !== 'number') {
-      return undefined;
-    }
+    if (typeof array[index] !== 'number') { return undefined; }
     sum += array[index];
   }
   return Math.round(sum / array.length);
