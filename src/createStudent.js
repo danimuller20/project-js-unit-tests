6c49c332d53cef404847045bce6bfd0c75d1aaec
Pedro Henrique <pedro.876.ph@gmail.com>
2021-01-17 23:05:17 -0300

    createStudent.js

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,8 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {
-
+const createStudent = (nome) => {
+  const object = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return object;
 };
 
 module.exports = createStudent;
