e6b331e58068ffbacda6e7456b2127a07c05fb05
saulokirchmaier <saulokirchmaier@gmail.com>
2021-01-18 13:57:09 -0300

    createMenu ex. 10/11/12 - codeclimate

--- src/restaurant.js
+++ src/restaurant.js
@@ -95,14 +95,14 @@ const createMenu = (myMenu) => {
     order: function order(newOrder) { this.consumption.push(newOrder); },
     pay: function pay() {
       let total = 0;
-      this.consumption.forEach(item => {
+      this.consumption.forEach( (item) => {
         if (Object.keys(this.fetchMenu.food).includes(item)) total += this.fetchMenu.food[item];
         else total += this.fetchMenu.drink[item];
       });
       return total * 1.1;
-    }
-  }
+    },
+  };
   return menu;
-}
+};
 
 module.exports = createMenu;
