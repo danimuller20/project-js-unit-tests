bc63a6a48f0f1f23b6def7826b3a943acc0dd3c2
RodrigoNeoEx <draja.acc@gmail.com>
2021-01-18 14:05:33 -0300

    createStudent done

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (event) => {
+  const estudante = {
+    name: event,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return estudante;
+}
 
 module.exports = createStudent;
