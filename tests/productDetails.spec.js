9e3128b268f0df69a0ee2e21ca9c1c3a8f4acd89
ggaldino95 <ggaldino95@gmail.com>
2021-01-18 19:10:48 -0300

    [Task 6] Small code adjusments

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -43,6 +43,7 @@ describe('#productDetails', () => {
     assert.strictEqual(productDetails('Alcool Gel', 'Mascara').length, 2);
     assert.deepStrictEqual(typeof Object.values(productDetails('Alcool Gel', 'Mascara')), 'object');
     assert.notDeepStrictEqual(Object.values(productDetails('Alcool Gel', 'Mascara'))[0], Object.values(productDetails('Alcool Gel', 'Mascara'))[1]);
-    assert.deepStrictEqual(Object.values(Object.values(productDetails('Alcool Gel', 'Mascara')[0])[1])[0].slice(-3) && Object.values(Object.values(productDetails('Alcool Gel', 'Mascara')[1])[1])[0].slice(-3) , '123');
+    assert.deepStrictEqual((Object.values(Object.values(productDetails('Alcool Gel', 'Mascara')[0])[1])[0].slice(-3)), '123');
+    assert.deepStrictEqual((Object.values(Object.values(productDetails('Alcool Gel', 'Mascara')[1])[1])[0].slice(-3)), '123');
   });
 });
