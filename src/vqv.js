6ce25388fb40d48b8743db1a87f793d8d4a4adbd
Poeta <pedrotrybe@gmail.com>
2021-01-18 15:16:42 -0300

    Code climete

--- src/vqv.js
+++ src/vqv.js
@@ -18,9 +18,7 @@
 */
 
 const vqv = (nome, idade) => {
-
-  if (nome && idade ) {
-return `Oi, meu nome é ${nome}!
+  if (nome && idade) {return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
 #VQV!`;
