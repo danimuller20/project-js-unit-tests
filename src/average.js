1dd8b2f800d67e20fa02599d272e2d89fab97720
analuizams <analuizamsalgado@gmail.com>
2021-01-18 05:58:50 -0300

    Adiciona requisito 1

--- src/average.js
+++ src/average.js
@@ -12,7 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
-//
-//
+const average = (numArray) => {
+  let result = 0;
+  if (numArray.length < 1) {
+    return undefined;
+  }
+  for (let index = 0; index < numArray.length; index +=1) {
+    if (typeof numArray[index] !== 'number') {
+      return undefined;
+    }
+  }
+  for (let index2 = 0; index2 < numArray.length; index2 +=1) {
+    result += numArray[index2]
+  }
+  result = Math.round(result/numArray.length);
+  return result;
+};
+
+// sobre Math.round: https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Math/round
+
 module.exports = average;
