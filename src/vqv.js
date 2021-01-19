13f5e6d61167b097cf3a1fbc2c730da7aa6eda73
cleber-teixeira <cleberlt@gmail.com>
2021-01-19 19:30:28 -0300

    Refatoração 03 - Requisito 03. :white_check_mark:

--- src/vqv.js
+++ src/vqv.js
@@ -16,9 +16,15 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+const vqv = (nome, idade) => {
+  if(vqv !== undefined) {
+    return `Oi, meu nome é ${nome}!
+    Tenho ${idade} anos,
+    trabalho na Trybe e mando muito em programação!
+    #VQV!`;
+  } else {
+    return undefined;
+  }
+}
 
 module.exports = vqv;
