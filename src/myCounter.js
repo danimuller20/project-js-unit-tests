9b441ee91e94f989464fd76f3ef60f7091c764e6
Igor Machado <rf59imft@gmail.com>
2021-01-19 10:51:27 -0300

    desafio my counter tentativa 13

--- src/myCounter.js
+++ src/myCounter.js
@@ -15,20 +15,18 @@
   myCounter() // Retorna: [0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3];
 */
 
-
 const myCounter = () => {
   var myArray = [];
   for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
     for (var counter = 2; counter <= 3; counter += 1) {
       myArray.push(counter);
-    for (let counter2 = 2; counter2 <= 3; counter2 += 1) {
-      myArray.push(counter2);
+    for (let counterTwo = 2; counterTwo <= 3; counterTwo += 1) {
+      myArray.push(counterTwo);
     }
   }
   return myArray;
 };
-
 module.exports = myCounter;
-
