1305e0e6a192e2fbb173a58afd1aec1bf96137b8
Sabrina A <sasa2605.alves@gmail.com>
2021-01-19 11:13:25 -0300

    Restart of project

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,19 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  if (typeof idade !== 'number') {
+    return undefined
+  }
+  else {
+    let phrase =
+      `Oi, meu nome é ${nome}!
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`
+    return phrase
+  }
+};
+console.log(vqv('sabrina', 'casa'));
 
 module.exports = vqv;
