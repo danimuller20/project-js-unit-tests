933fa604c83b693229075d932e27073fcb657541
Marina Alane <marina.alane@outlook.com>
2021-01-19 11:01:26 -0300

    ajusta requisitos

--- src/productDetails.js
+++ src/productDetails.js
@@ -25,18 +25,7 @@
 */
 
 const productDetails = (firstProduct, secondProduct) => [
-  {
-    name: firstProduct,
-    details: {
-      productId: `${firstProduct}123`,
-    },
-  },
-  {
-    name: secondProduct,
-    details: {
-      productId: `${secondProduct}123`,
-    },
-  },
+  
 ];
 
 module.exports = productDetails;
