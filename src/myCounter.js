afced74960a234ce28d56129abb9bb8abbb17441
brunamfguimaraes <brunamfguimaraes@gmail.com>
2021-01-19 11:39:30 -0300

    code climate issues

--- src/myCounter.js
+++ src/myCounter.js
@@ -17,12 +17,12 @@
 
 const myCounter = () => {
   var myArray = [];
-    for (var counter = 0; counter <= 9; counter += 1) {
+  for (var counter = 0; counter <= 3; counter += 1) {
       myArray.push(counter);
-    for (var counter1 = 2; counter1 <= 3; counter1 += 1) {
+      for (var counter1 = 2; counter1 <= 3; counter1 += 1) {
       myArray.push(counter1);
     }
-  }
+    }
   return myArray;
 };
 console.log(myCounter([0, 2, 3, 1, 2, 3, 2, 2, 3, 3, 2, 3]));
