7a0c6b633e53bd57f399adb03da785468a5c8447
Cristiano Roberto <cristianoroberto@outlook.com>
2021-01-19 18:56:55 -0300

    Corrigido linha de código vqv, pelo npm test

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -28,8 +28,11 @@ describe('#circle', () => {
     assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
+    assert.deepStrictEqual(typeof circle(1), 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.deepStrictEqual(Object.keys(circle(1).lenght, 3));
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
