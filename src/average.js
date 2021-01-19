3a5a9bb1a0d745bf01fe0e591f8f4f9a854ec677
santiago <felipesantiagolds@gmail.com>
2021-01-19 10:54:02 -0300

    Correção averange

--- src/average.js
+++ src/average.js
@@ -16,15 +16,15 @@ const average = (array) => {
   let total = 0;
 
   for (let index = 0; index < array.length; index += 1) {
-    if (typeof i !== 'number') {
+    if (typeof array[index] !== 'number') {
       return undefined;
     }
-    if (i.length === 0) {
+    if (array[index].length === 0) {
       return undefined;
     }
-    total += i;
+    total += array[index];
   }
   return Math.round(total / array.length);
 };
-
+console.log(average([1, 2, 3, 4, 5]));
 module.exports = average;
