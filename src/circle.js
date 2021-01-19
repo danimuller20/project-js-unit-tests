13a63a640d6f8a92b7475708202eb151b17275a2
Matheus Martino <matheus.martino97@gmail.com>
2021-01-19 20:04:58 -0300

    Solving problems with the circle.spec.js tests.

--- src/circle.js
+++ src/circle.js
@@ -25,9 +25,11 @@ const circle = (radius) => {
   }
   return {
     radius,
-    area: PI * radius * radius,
-    circumference: 2 * PI * radius,
+    area: parseFloat((PI * radius * radius).toPrecision(4)),
+    circumference: parseFloat((2 * PI * radius).toPrecision(4)),
   };
 };
 
 module.exports = circle;
+
+console.log(circle(3))
