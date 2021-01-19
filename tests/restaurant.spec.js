fb59f6f7eacdbae9ac4e588aea50818842a48017
Rafael Azevedo <rafaelazevedo321@gmail.com>
2021-01-19 17:21:21 -0300

    corrigido codeclimate

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -64,8 +64,8 @@ describe('#createMenu', () => {
     // ```
     // const objetoRetornado = createMenu(objetoQualquer);
     // objetoRetornado.fetchMenu // Retorno: { food: {}, drink: {}}
-    const menu = createMenu({ food: {}, drink: {} });
-    assert.deepStrictEqual(menu.fetchMenu, { food: {}, drink: {} });
+    const menu = createMenu({ food: {}, drink: {}});
+    assert.deepStrictEqual(menu.fetchMenu, { food: {}, drink: {}});
 
     // ```
     // Agora faça o TESTE 3 deste arquivo.
