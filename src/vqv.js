f22a2d577f62c4a36d44878e40470b4b3ca01372
Lucas Silva <martssilvapro@gmail.com>
2021-01-20 09:19:30 -0300

    correcao das templates string

--- src/vqv.js
+++ src/vqv.js
@@ -18,12 +18,12 @@
 
 const vqv = (nome, idade) => {
   if (nome && idade) {
-    return `Oi, meu nome é ${nome}! 
-  Tenho ${idade} anos, 
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+    return `  Oi, meu nome é ${nome}! 
+Tenho ${idade} anos, 
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
   }
   return undefined;
 };
-
+console.log(vqv("Lucas", 20))
 module.exports = vqv;
