d1ac566f9aab9a2f4f395bb0c511912ce3e47dc7
fernandacajueiro <fernandacajueiromachado@gmail.com>
2021-01-20 12:24:11 -0300

    Last Code Climate issues

--- src/restaurant.js
+++ src/restaurant.js
@@ -71,7 +71,7 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-/* 
+/*
 const myMenu = {
   food: { coxinha: 3.9, sopa: 9.9, sashimi: 13 },
   drink: { agua: 3.9 },
@@ -92,7 +92,7 @@ const createMenu = myMenu =>
         }
       });
       return parseFloat((total * 1.10).toPrecision(4));
-    }
+    },
   });
 
 module.exports = createMenu;
