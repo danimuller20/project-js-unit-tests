fe95c11575b3b7debcedf39869c30960877b8840
Matheus Martino <matheus.martino97@gmail.com>
2021-01-20 19:00:05 -0300

    Solving the first step of the restaurant.js.

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-
+assert = require('assert')
 /*
   Você é responsável por escrever o código do sistema de pedidos de um restaurante. Deve ser possível, através desse sistema, cadastrar um menu. Dado que um menu foi cadastrado, o sistema deve disponibilizar um objeto através do qual se consegue:
   - ler o menu cadastrado;
@@ -71,6 +71,10 @@
 
 // PASSO 4: Adicione ao objeto retornado por `createMenu()` uma chave `pay` com uma função que varre todo os itens de `objetoRetornado.consumption`, soma o preço de todos checando-os no menu e retorna o valor somado acrescido de 10%. DICA: para isso, você precisará varrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+const createMenu = (object) => {
+  return {
+    fetchMenu: object
+  }
+};
 
 module.exports = createMenu;
