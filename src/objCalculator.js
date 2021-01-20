b46a4854c92025f7cf9899cf49d457597298bb76
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-01-20 11:29:31 -0300

    CodeClimate

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -20,12 +20,10 @@
 */
 
 const calculator = {
-  add: (number1,number2) => Math.trunc(number1 + number2),
-  mult: (number1,number2) => Math.trunc(number1 * number2),
-  div: (number1,number2) => Math.trunc(number1 / number2),
-  sub: (number1,number2) => Math.trunc(number1 - number2),
+  add: (number1, number2) => Math.trunc(number1 + number2),
+  mult: (number1, number2) => Math.trunc(number1 * number2),
+  div: (number1, number2) => Math.trunc(number1 / number2),
+  sub: (number1, number2) => Math.trunc(number1 - number2),
 };
-
-console.log(calculator.div(10,3))
 
 module.exports = calculator;
