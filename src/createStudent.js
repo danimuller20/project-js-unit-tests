52fcf0ac0c93c4fa5647f59fc25ba61093b762f3
Nathália <nathaliaveneziano@icloud.com>
2021-01-20 09:42:57 -0300

    Changes codeclimate

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,9 +15,9 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = (name) => {
+const createStudent = (nameUser) => {
   const dataStudent = {
-    name: name,
+    name: nameUser,
     feedback: () => 'Eita pessoa boa!',
   };
 
