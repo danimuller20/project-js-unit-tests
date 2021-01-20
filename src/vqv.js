0044a154cedbf17d8d753c2a425242609a08636b
Matheus Martino <matheus.martino97@gmail.com>
2021-01-20 11:39:09 -0300

    Making the vqv funciton.

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,13 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const message = 'Oi, meu nome é ' + nome + '!\n' +
+  'Tenho ' + idade + ' anos,\n' +
+  'trabalho na Trybe e mando muito em programação!\n' +
+  '#VQV!'
+
+  return message
+};
 
 module.exports = vqv;
