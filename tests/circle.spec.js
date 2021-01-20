0625804cf39890976403c26a8f0d30890e4990d1
Jhonatan Arguello <jhonatan.arguello@gmail.com>
2021-01-20 20:29:33 -0300

    Finalizando os testes da funçao productDetails.

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -29,8 +29,9 @@ describe('#circle', () => {
     // Teste se circle retorna um objeto.
     assert.strictEqual(typeof circle(7), 'object')
     // Teste se o objeto retornado tem 3 entradas.
-    assert.strictEqual(Object.keys.length, length, 3)
+    assert.strictEqual(Object.keys(circle(7)).length, 3)
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined)
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
