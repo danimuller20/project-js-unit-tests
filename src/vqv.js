09a51e546df6d85ad71b3172d69093950d430ddc
Lucas Silva <martssilvapro@gmail.com>
2021-01-20 08:51:40 -0300

    correcao na logica qvq

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-  if (nome && idade === '') {
-    return undefined;
+  if (nome && idade) {
+    return `Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação! #VQV`;
+
   }
-  return `Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação! #VQV`;
+  return undefined;
 };
 
 module.exports = vqv;
