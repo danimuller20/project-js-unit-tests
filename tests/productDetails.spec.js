4b2ced6c81b214a6ad14c82a4ba1492c49b785a0
Enio Nicael <enionicael@gmail.com>
2021-01-20 18:54:19 -0300

    correcoes do code climate.

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,11 +33,17 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste que o retorno da função é um array.
+    const productDetailsFunc = productDetails('Leite', 'Biscoito');
+    assert.strictEqual(Array.isArray(productDetailsFunc), true);
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(Object.keys(productDetailsFunc).length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    const contentIsObject = () => {
+      return productDetailsFunc.every((product) => typeof product === 'object');
+    }
+    assert.strictEqual()
     // Teste que os dois objetos são diferentes entre si.
     // (Difícil) Teste que os dois productIds terminam com 123.
   });
