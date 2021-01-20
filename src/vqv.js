7ec3d997589fd23539934b0641e910258396c792
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-01-20 19:32:33 -0300

    finalmente descobri o que estava dando errado

--- src/vqv.js
+++ src/vqv.js
@@ -19,11 +19,10 @@
 const vqv = (nome, idade) => {
   const name = nome;
   const age = idade;
-
-  if (!name && !age) {
+  if (!name || !age) {
     return undefined;
   }
-  return `Oi, meu nome é ${name}! Tenho ${age} anos, trabalho na Trybe e mando muito em programação! #VQV!`;
+  return `Oi, meu nome é ${name}!\nTenho ${age} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
 };
 
 module.exports = vqv;
