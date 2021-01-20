fac09a0a81a2374cb905b33b6381f1c21bbc7be3
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-01-20 19:06:15 -0300

    tentativa de passar o codigo

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+  const name = nome;
+  const age = idade;
+
+  if (!name && !age) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${name}!\.Tenho ${age} anos,\.trabalho na Trybe e mando muito em programação!\.#VQV!`;
+};
 
 module.exports = vqv;
