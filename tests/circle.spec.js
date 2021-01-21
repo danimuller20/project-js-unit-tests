1309aa614f8fdb59802b7c7a548ff2c32d68c26a
Ricardo Sbeghen <ricardo.sbeghen@gmail.com>
2021-01-21 09:41:44 -0300

    Exercice 2 - 6/6

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -40,7 +40,9 @@ describe('#circle', () => {
     assert.strictEqual((circle(2)).circumference, 2 * 3,14 * 2);
 
     //5 Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
-    
+    assert.strictEqual((circle(2)).area, 3,14 * 3 * 3);
+
     //6 Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
+    assert.strictEqual((circle(3)), { 'radius': 3, 'area':3,14 * 3 * 3, 'circumference': 2 * 3,14 * 3})
   });
 });
