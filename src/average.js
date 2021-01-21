23cc0557f2df88ce8ce237b956a83fb906a5081d
pauloeliezerg <pauloeliezerg@gmail.com>
2021-01-21 07:31:28 -0300

    Added average function

--- src/average.js
+++ src/average.js
@@ -12,6 +12,19 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (array) => {
+  let result = 0;
+  if (!array.length) {
+    return undefined;
+  }
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] !== 'number') {
+      return undefined;
+    }
+    result += array[index];
+  }
+  result = Math.round(result / array.length);
+  return result;
+};
 
 module.exports = average;
