631326bfabd0f0f70dea3006b83d3019f183c58b
Gabriel Medeiros <gabrielmarkyan@gmail.com>
2021-01-21 12:51:27 -0300

    Implementation of test cases for productDetails function - Requirement 6

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -33,12 +33,10 @@ const productDetails = require('../src/productDetails');
 
 describe('#productDetails', () => {
   it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.strictEqual(Array.isArray(productDetails('Alcool gel', 'Mascara')), true); // Teste que o retorno da função é um array.
+    assert.strictEqual(productDetails('Alcool gel', 'Mascara').length, 2); // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(typeof Object.values(productDetails('Alcool gel', 'Mascara')), 'object'); // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.notDeepStrictEqual(Object.values(productDetails('Alcool gel', 'Mascara'))[1], Object.values(productDetails('Alcool gel', 'Mascara'))[2]); // Teste que os dois objetos são diferentes entre si.
+    assert.deepStrictEqual(Object.values(productDetails('Alcool gel', 'Mascara'))[0].details.productId.endsWith('123'), Object.values(productDetails('Alcool gel', 'Mascara'))[1].details.productId.endsWith('123')); // (Difícil) Teste que os dois productIds terminam com 123.
   });
 });
