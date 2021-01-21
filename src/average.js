327dd5a6ed5380b86fb279689933b5df123efc3c
Fernando Resende <fernandocastellob@gmail.com>
2021-01-21 18:52:19 -0300

    average, student, calculator

--- src/average.js
+++ src/average.js
@@ -12,7 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = (nums) => nums.reduce((a, b) => (a + b)) / nums.length;
-
-
+const average = (array) => {
+  if (array.length === 0) {
+    return undefined
+  }
+  let sum = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (typeof array[index] === 'number') {
+      sum = sum + array[index];
+    } else {
+      return undefined
+    }
+  }
+  return Math.round(sum / array.length);
+};
 module.exports = average;
