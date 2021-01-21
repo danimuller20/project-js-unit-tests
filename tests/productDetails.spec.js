3e96dd2b39a249ad82f6cb78e277bdee11ffccf7
ederson rodrigo <edersonrodrigo31@gmail.com>
2021-01-21 11:12:05 -0300

    funcao productdetails e circle fix

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -37,8 +37,11 @@ describe('#productDetails', () => {
     // Teste que o retorno da função é um array.  
     assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara'), 'array');
     // Teste que o array retornado pela função contém dois itens dentro.
+    assert.strictEqual(productDetails('Alcool gel', 'Máscara').length, 2);
     // Teste que os dois itens dentro do array retornado pela função são objetos.
+    assert.strictEqual(typeof productDetails('Alcool gel', 'Máscara').keys, 'object');
     // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
+    assert.notStrictEqual(productDetails('Alcool gel', 'Máscara')[0], productDetails('Alcool gel', 'Máscara')[1]);
+    // (Difícil) Teste que os dois productIds terminam com 123.;
   });
 });
