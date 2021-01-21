f4a2e0d4376201ed763c0187f2bbefa01c1b3764
Rodrigo Farias <deltadigo@gmail.com>
2021-01-20 22:56:12 -0300

    Correções do codeclimate do requisito 3

--- src/vqv.js
+++ src/vqv.js
@@ -20,11 +20,10 @@ const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) {
     return undefined;
   }
-  let phrase = `Oi, meu nome é ${nome}!
+  return `Oi, meu nome é ${nome}!
 Tenho ${idade} anos,
 trabalho na Trybe e mando muito em programação!
-#VQV!`
-  return phrase;
+#VQV!`;
 };
 
 module.exports = vqv;
