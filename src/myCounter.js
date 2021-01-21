8c753949ac0417137f3de8a262fc95a255793922
MichaelMascarenhas <michaelmascarenhastrybe@gmail.com>
2021-01-21 13:52:36 -0300

    alteraçoes de var para const ou let, e retirada repetiçao do indice do laço

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  var myArray = [];
-  for (var counter = 0; counter <= 3; counter += 1) {
+  const myArray = [];
+  for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (var counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let outherCounter = 2; outherCounter <= 3; outherCounter += 1) {
+      myArray.push(outherCounter);
     }
   }
   return myArray;
