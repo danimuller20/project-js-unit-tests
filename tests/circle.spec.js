85dd5715241b2a5825eadd1b2b8e68dce1f7f0aa
Lucas Silva <martssilvapro@gmail.com>
2021-01-21 20:54:38 -0300

    teste

--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -37,6 +37,5 @@ describe('#circle', () => {
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
     assert.strictEqual(circle(3).area.toPrecision(3), '28.3')
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
-    assert.strictEqual(circle(3).area.toPrecision(3),)
   });
 });
