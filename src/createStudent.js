7606da27233ed6ff64c87f31608d50fdc75192f3
Hudson Carlos <hc88806@gmail.com>
2021-01-21 08:43:42 -0400

    corrigindo codeclimate

--- src/createStudent.js
+++ src/createStudent.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 /*
   Dada uma função chamada createStudent que recebe como parâmetro um nome,
   retorne um objeto que contenha duas chaves:
@@ -22,21 +21,4 @@ const createStudent = (nomeEstudante) => {
     feedback: () => 'Eita pessoa boa!'
   };
 };
-   const estudante = createStudent('Leandrão, o Lobo Solitário');
-    assert.strictEqual(typeof estudante, 'object');
-    assert.strictEqual(typeof estudante.feedback, 'function');
-    assert.strictEqual(estudante.name, 'Leandrão, o Lobo Solitário');
-    assert.strictEqual(estudante.feedback(), 'Eita pessoa boa!');
-
-    const estudante2 = createStudent('Nobre');
-    assert.strictEqual(typeof estudante2, 'object');
-    assert.strictEqual(typeof estudante2.feedback, 'function');
-    assert.strictEqual(estudante2.name, 'Nobre');
-    assert.strictEqual(estudante2.feedback(), 'Eita pessoa boa!');
-
-    const estudante3 = createStudent('Inácio');
-    assert.strictEqual(typeof estudante3, 'object');
-    assert.strictEqual(typeof estudante3.feedback, 'function');
-    assert.strictEqual(estudante3.name, 'Inácio');
-    assert.strictEqual(estudante3.feedback(), 'Eita pessoa boa!');
-//module.exports = createStudent;
+module.exports = createStudent;
