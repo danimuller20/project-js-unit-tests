b3577fae7064adac23d5359c5ee0387878a4f85e
Danrlley Rodrigues <danrlley.c.a.rodrigues@gmail.com>
2021-01-21 15:11:47 -0300

    Requisitos Corrigidos 2

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,9 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = nome => ({
-  nome,
-  feedback: () => 'Eita pessoa boa!',
-});
+const createStudent = (nome) => {
+  const obj = {
+    name: nome,
+    feedback: () => 'Eita pessoa boa!',
+  };
+  return obj;
+};
 
 module.exports = createStudent;
