19430eaf7ead87885dde3271d119c874fd27f999
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-01-21 12:08:57 -0300

    vqv alinhando

--- src/vqv.js
+++ src/vqv.js
@@ -18,7 +18,7 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) { return undefined; }
-  let phrase = `Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação!#VQV!`;
+  const phrase = `Oi, meu nome é ${nome}! Tenho ${idade} anos, trabalho na Trybe e mando muito em programação!#VQV!`;
   return phrase;
 };
 
