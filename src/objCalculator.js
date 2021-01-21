ceefdf4b35c8ecc56e62fea89380e851aa2c10a0
alailsonko <aalailson3@gmail.com>
2021-01-21 06:18:10 -0300

    test calculator

--- src/objCalculator.js
+++ src/objCalculator.js
@@ -21,7 +21,7 @@
 const calculator = {
   add: (a, b) => a + b,
   mult: (a, b) => a * b,
-  div: (a, b) => a / b,
+  div: (a, b) => parseInt(a / b),
   sub: (a, b) => a - b,
 };
 module.exports = calculator;
