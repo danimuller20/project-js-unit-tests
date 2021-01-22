b7d70d1ed90f38376b1e6972cbab3d5295c6abda
otacisos <otacisods@gmail.com>
2021-01-22 01:27:34 -0300

    Requisito VQV fix 9

--- src/vqv.js
+++ src/vqv.js
@@ -17,10 +17,12 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof nome === 'undefined' || typeof idade === 'undefined') return undefined;
+  if (typeof nome === 'undefined' || typeof idade === 'undefined') {
+    return undefined;
+  }
   return `Oi, meu nome é ${nome}!
-   Tenho ${idade} anos,
-   trabalho na Trybe e mando muito em programação!
-   #VQV!'`;
+Tenho ${idade} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
 };
 module.exports = vqv;
