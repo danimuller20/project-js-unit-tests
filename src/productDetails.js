28b6d2b3f8ba6c8ac52050f4bb32eb2510b710eb
Marina Alane <marina.alane@outlook.com>
2021-01-22 13:28:24 -0300

    corrige cc

--- src/productDetails.js
+++ src/productDetails.js
@@ -27,16 +27,16 @@
  const productDetails = (firstProduct, secondProduct) =>
    [
      {
-       name: firstProduct,
-       details: {
-        productId: `${firstProduct}123`,
+        name: firstProduct,
+        details: {
+         productId: `${firstProduct}123`,
       },
      },
      {
-       name: secondProduct,
-       details: {
-        productId: `${secondProduct}123`,
-      },
-     },
+        name: secondProduct,
+        details: {
+         productId: `${secondProduct}123`,
+       },
+   },
     ];
  module.exports = productDetails;
