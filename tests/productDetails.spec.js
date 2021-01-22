e9abb3c77207d89a9f349594ee356f03109bb444
Renato Benage <renatobenage@gmail.com>
2021-01-22 13:30:09 -0300

    funçao

--- tests/productDetails.spec.js
+++ tests/productDetails.spec.js
@@ -31,14 +31,32 @@ const productDetails = require('../src/productDetails');
   OBS: Lembre-se que você não precisa se preocupar com o describe e o it por enquanto, isso será aprendido posteriormente.
 */
 
-describe('#productDetails', () => {
-  it('tests the function has the correct behaviour', () => {
-    assert.fail();
-    // ESCREVA SEUS TESTES ABAIXO:
-    // Teste que o retorno da função é um array.
-    // Teste que o array retornado pela função contém dois itens dentro.
-    // Teste que os dois itens dentro do array retornado pela função são objetos.
-    // Teste que os dois objetos são diferentes entre si.
-    // (Difícil) Teste que os dois productIds terminam com 123.
-  });
-});
+// describe('#productDetails', () => {
+//   it('tests the function has the correct behaviour', () => {
+//     assert.fail();
+//     // ESCREVA SEUS TESTES ABAIXO:
+//     // Teste que o retorno da função é um array.
+//     // Teste que o array retornado pela função contém dois itens dentro.
+//     // Teste que os dois itens dentro do array retornado pela função são objetos.
+//     // Teste que os dois objetos são diferentes entre si.
+//     // (Difícil) Teste que os dois productIds terminam com 123.
+//   });
+// });
+// MEU CODIGO
+
+const productDetails = (firstProduct, secondProduct) => [
+  {
+    name: firstProduct,
+    details: {
+      productId: `${firstProduct}123`,
+    },
+  },
+  {
+    name: secondProduct,
+    details: {
+      productId: `${secondProduct}123`,
+    },
+  },
+];
+
+module.exports = productDetails;
