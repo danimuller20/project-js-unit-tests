9579013d4ed3c4797d214c92f337cc131c1f5072
Marina Alane <marina.alane@outlook.com>
2021-01-22 12:38:45 -0300

    ajusta cc

--- src/circle.js
+++ src/circle.js
@@ -19,9 +19,9 @@
 */
 
  const circle = (radius) => {
-  const PI = 3.14;
-  if (!radius) { return undefined; }
-  return {
+   const PI = 3.14;
+   if (!radius) { return undefined; }
+   return {
     radius,
     area: PI * radius * radius,
     circumference: 2 * PI * radius,
