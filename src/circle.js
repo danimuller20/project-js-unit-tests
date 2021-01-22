a3c9fdbfcb653e7ae9a16e856e8ee54014c001cc
Marina Alane <marina.alane@outlook.com>
2021-01-22 12:40:43 -0300

    corrige cc

--- src/circle.js
+++ src/circle.js
@@ -22,9 +22,9 @@
    const PI = 3.14;
    if (!radius) { return undefined; }
    return {
-    radius,
-    area: PI * radius * radius,
-    circumference: 2 * PI * radius,
-  };
+     radius,
+     area: PI * radius * radius,
+     circumference: 2 * PI * radius,
+   };
  };
  module.exports = circle;
