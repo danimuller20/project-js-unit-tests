d1dc92be81b1ad641ca16c598e7f1d27e04803e2
FredericoCanoeira <fcanoeira@gmail.com>
2021-01-22 03:00:22 +0100

    resolve segundo requisito tentativa1

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,14 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+const vqv = (nome, idade) => {
+if  (nome ) {
+
+}
+  return `Oi, meu nome é ${nome}!\n
+      Tenho ${idade} anos,
+      trabalho na Trybe e mando muito em programação!
+      #VQV!`
+};
 
 module.exports = vqv;
