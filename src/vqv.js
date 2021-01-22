fbdff0d77d6832dd4c8e5d8b8a1af5648dd13f99
Emanoel Leão <emanuelleaomarinho@gmail.com>
2021-01-21 23:58:06 -0300

    Alinhando fução VQV

--- src/vqv.js
+++ src/vqv.js
@@ -18,10 +18,7 @@
 
 const vqv = (nome, idade) => {
   if (nome === undefined || idade === undefined) { return undefined; }
-  const phrase = `Oi, meu nome é ${nome}!
-  Tenho ${idade} anos,
-  trabalho na Trybe e mando muito em programação!
-  #VQV!`;
+  const phrase = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n #VQV!`;
   return phrase;
 };
 
--- tests/circle.spec.js
+++ tests/circle.spec.js
@@ -25,14 +25,17 @@ const circle = require('../src/circle');
 
 describe('#circle', () => {
   it('given a radius, should return an object with circles info', () => {
-    assert.fail();
     // ESCREVA SEUS TESTES ABAIXO:
     // Teste se circle retorna um objeto.
-    assert.strictEqual(typeof circle, 'function');
+    assert.strictEqual(typeof circle, 'object');
     // Teste se o objeto retornado tem 3 entradas.
+    assert.strictEqual(Object.keys(circle(2).length, 3));
     // Teste se a função, quando não recebe nenhum parâmetro, retorna undefined.
+    assert.strictEqual(circle(), undefined);
     // Teste que a função retorna, dentro de um objeto, a circunferência correta para um círculo de raio 2.
+    assert.strictEqual(circle(2).circumference,)
     // Teste que a função retorna, dentro de um objeto, a área correta para um círculo de raio 3.
+    assert.strictEqual
     // Teste que a função retorna, num objeto, os dados corretos de um círculo de raio 3.
   });
 });
