e6fa9115377bbfb361219516ce89c03b0c741218
Renato Benage <renatobenage@gmail.com>
2021-01-22 12:00:22 -0300

    vqv

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,16 @@
       #VQV!'
 */
 
-const vqv = (nome, idade) => {};
+// const vqv = (nome, idade) => {};
+
+// module.exports = vqv;
+// MEU CODIGO
+
+const vqv = (nome, idade) => {
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  return `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+};
 
 module.exports = vqv;
