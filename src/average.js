2a30877743d179f0f4d571bfb07f738748d025ff
Renato Benage <renatobenage@gmail.com>
2021-01-22 11:48:45 -0300

    average

--- src/average.js
+++ src/average.js
@@ -12,6 +12,18 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {};
+const average = (teste) => {
+  let sum = 0;
+  if (teste.length === 0) {
+    return undefined;
+  }
+  for (let index = 0; index < teste.length; index += 1) {
+    if (typeof (teste[index]) !== 'number') {
+      return undefined;
+    }
+    sum += teste[index];
+  }
+  return Math.round(sum / teste.length);
+};
 
 module.exports = average;
