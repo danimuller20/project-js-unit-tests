471062eb73fbc5ed3ff479f5e02e26f1022decc5
MZTBTG <mztbtg@hotmail.com>
2021-01-22 13:41:26 -0300

    myCounter.js Correção 2

--- src/myCounter.js
+++ src/myCounter.js
@@ -16,11 +16,11 @@
 */
 
 const myCounter = () => {
-  let myArray = [];
+  const myArray = [];
   for (let counter = 0; counter <= 3; counter += 1) {
     myArray.push(counter);
-    for (let counter = 2; counter <= 3; counter += 1) {
-      myArray.push(counter);
+    for (let insideCounter = 2; insideCounter <= 3; insideCounter += 1) {
+      myArray.push(insideCounter);
     }
   }
   return myArray;
