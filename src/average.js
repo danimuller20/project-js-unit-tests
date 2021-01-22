0fe013a9bbe7ebf459eec3af7ead34267ae6e829
smylle <jjsilveira007@gmail.com>
2021-01-22 09:47:34 -0300

    Average function

--- src/average.js
+++ src/average.js
@@ -12,8 +12,11 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  
+const average = (array) => {
+  let soma = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    soma += array[index] /array.length;    
+  }
 };
 
 module.exports = average;
