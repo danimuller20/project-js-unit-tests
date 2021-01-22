53f193cdd69c9525e99f1022d962e4db624d1fbb
otacisos <otacisods@gmail.com>
2021-01-22 02:12:35 -0300

    CodeclimateCry3

--- src/createStudent.js
+++ src/createStudent.js
@@ -17,8 +17,8 @@
 
 const createStudent = (nome) => {
   const student = {
-  name: nome,
-  feedback: () => 'Eita pessoa boa!',
+    name: nome,
+    feedback: () => "Eita pessoa boa!",
   };
   return student;
 };
