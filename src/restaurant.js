10f9030bce98ffedb2c22d3dcee0d6a5e7abc97f
Hudson Carlos <hc88806@gmail.com>
2021-01-23 16:50:31 -0400

    corrifindo codeclimate

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,17 +1,17 @@
 const createMenu = (objeto) => {
   let total = 0;
-   myMenu = {
-    fetchMenu: objeto,
-    consumption: [],
-    order: (addItem) => myMenu.consumption.push(addItem),
-    pay: () => {
-      for (let index = 0; index < myMenu.consumption.length; index += 1) {
-        for (let x = 0; x < Object.keys(myMenu.fetchMenu.food).length; x += 1) {
-          if (myMenu.consumption[index] === Object.keys(myMenu.fetchMenu.food)[x]) {
-            total += Object.values(myMenu.fetchMenu.food)[x];
-          }
+  myMenu = {
+  fetchMenu: objeto,
+  consumption: [],
+  order: (addItem) => myMenu.consumption.push(addItem),
+  pay: () => {
+    for (let index = 0; index < myMenu.consumption.length; index += 1) {
+      for (let x = 0; x < Object.keys(myMenu.fetchMenu.food).length; x += 1) {
+        if (myMenu.consumption[index] === Object.keys(myMenu.fetchMenu.food)[x]) {
+          total += Object.values(myMenu.fetchMenu.food)[x];
         }
-        for (let x1 = 0; x1 < Object.keys(myMenu.fetchMenu.drink).length; x1 += 1) {
+      }
+      for (let x1 = 0; x1 < Object.keys(myMenu.fetchMenu.drink).length; x1 += 1) {
           if (myMenu.consumption[index] === Object.keys(myMenu.fetchMenu.drink)[x1]) {
             total += Object.values(myMenu.fetchMenu.drink)[x1];
           }
