c770420b7d3a38436fc1d38c05096b27f220b191
Hudson Carlos <hc88806@gmail.com>
2021-01-23 18:49:21 -0400

    corrigindo codeclimate

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,21 +1,25 @@
-let total = 0;
+const sumMenu = () => {
+  let total = 0;
+  for (let index = 0; index < myMenu.consumption.length; index += 1) {
+    if (Object.keys(myMenu.fetchMenu.food).includes(myMenu.consumption[index])) {
+      total += myMenu.fetchMenu.food[myMenu.consumption[index]];
+    }
+    if (Object.keys(myMenu.fetchMenu.drink).includes(myMenu.consumption[index])) {
+      total += myMenu.fetchMenu.drink[myMenu.consumption[index]];
+    }
+  }
+  total += (total * 0.10);
+  return total.toFixed(2);
+};
+
+
+
 const createMenu = (objeto) => {
   myMenu = {
     fetchMenu: objeto,
     consumption: [],
     order: addItem => myMenu.consumption.push(addItem),
-    pay: () => {
-      for (let index = 0; index < myMenu.consumption.length; index += 1) {
-        if (Object.keys(myMenu.fetchMenu.food).includes(myMenu.consumption[index])) {
-          total += myMenu.fetchMenu.food[myMenu.consumption[index]];
-        }
-        if (Object.keys(myMenu.fetchMenu.drink).includes(myMenu.consumption[index])) {
-          total += myMenu.fetchMenu.drink[myMenu.consumption[index]];
-        }
-      }
-      total += (total * 0.10);
-      return total.toFixed(2);
-    },
+    pay: sumMenu(),
   };
   return myMenu;
 };
