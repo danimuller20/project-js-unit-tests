40a972fb1acf8bd95c3b6007663be98d213b0589
Fabio Venancio Almeida <fvalmeida.@gmail.com>
2021-01-27 16:13:23 -0300

    Requisito 5

--- src/vqv.js
+++ src/vqv.js
@@ -17,7 +17,11 @@
 */
 
 const vqv = (nome, idade) => {
-
+  if (nome === undefined || idade === undefined) {
+    return undefined;
+  }
+  const phrase = `Oi, meu nome é ${nome}!\nTenho ${idade} anos,\ntrabalho na Trybe e mando muito em programação!\n#VQV!`;
+  return phrase;
 };
 
 console.log(vqv());
