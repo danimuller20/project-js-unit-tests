8b1925a2befcff1dc13377f10a2fa33e4ec0710c
Bruna <92brunacampos@gmail.com>
2021-02-01 14:44:40 -0300

    createStudent 1

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,13 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
-
+const createStudent = (name) => {
+  return {
+    name: name,
+    feedback: function () {
+      return 'Eita pessoa boa!';
+    },
+  };
+};
+console.log(createStudent('Leandrão, o Lobo Solitário'));
 module.exports = createStudent;
