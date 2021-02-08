1d544e577c40e8e8667cdd86838cf4feb170dc8d
Lenilsom Ferreira da Silva <silvalenilsomsud@gmail.com>
2021-02-08 16:30:28 -0300

    Adicionando teste numbers

--- src/average.js
+++ src/average.js
@@ -13,23 +13,23 @@
 */
 
 const average = (arrayOfNumbers) => {
-   if (arrayOfNumbers.length === 0) {
+ /*  if (arrayOfNumbers.length === 0) {
     return undefined;
   }
-  for (let index =  0; index <= arrayOfNumbers.length; index += 1) {
+  for (let index = 0; index <= arrayOfNumbers.length; index += 1) {
     if (typeof arrayOfNumbers[index] === 'string') {
       return undefined;
     }
   }
   let sum = 0;
-  for (let index = 0; index <= arrayOfNumbers.length ; index += 1){
-   if (typeof(arrayOfNumbers[index]) === 'number'){
+  for (let index = 0; index <= arrayOfNumbers.length; index += 1){
+    if (typeof (arrayOfNumbers[index]) === 'number'){
       sum = sum + arrayOfNumbers[index];
     }
   }
   const averageValue = sum / arrayOfNumbers.length;
   const averageValueRounded = Math.round(averageValue);
   console.log(averageValueRounded);
-};
+};*/
 
 module.exports = average;
