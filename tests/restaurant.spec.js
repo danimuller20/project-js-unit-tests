7f7f900b210716606df7f8e0ed0ebb3fcad7aed7
Layo Kaminski <layokaminski@gmail.com>
2021-02-09 16:03:07 -0300

    Ajustes de conflito

--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -53,9 +53,8 @@ describe('#createMenu', () => {
   it('tests the function has the correct behaviour', () => {
     // TESTE 1: Verifique que, dado um objeto qualquer passado como um parâmetro para a função createMenu(), checa se o retorno da função é um objeto no seguinte formato: { fetchMenu: objetoQualquer }.
     // ```
-    // createMenu(objetoQualquer) // Retorno: { fetchMenu: objetoQualquer }
-    // `fetchMenu` deve ser uma função quer retorna o `objetoQualquer`
     // createMenu(objetoQualquer) // Retorno: { fetchMenu: () => objetoQualquer }  
+    // `fetchMenu` deve ser uma função quer retorna o `objetoQualquer`
     // ```
     assert.deepStrictEqual(createMenu({ menu: 'coxinha' }).fetchMenu, { menu: 'coxinha' });
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
