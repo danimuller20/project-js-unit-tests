0994f0553fd7fc721d84a9a372127c4924514de6
Pedro Castro <pa.coelho.castro@gmail.com>
2021-02-15 12:43:13 -0300

    New task 3 + task 1 with dynamic array length v5

--- src/average.js
+++ src/average.js
@@ -16,7 +16,7 @@ const average = (arr) => {
   if (arr.some(element => typeof (element) !== 'number') || arr.length === 0) {
     return undefined;
   } return Math.round(
-    arr.reduce((accumulator, currentValue) => (accumulator + currentValue) / arr.length),
+    arr.reduce((accumulator, currentValue) => accumulator + currentValue) / arr.length,
   );
 };
 
