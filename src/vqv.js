2dbc40db33a9e8b7986c2a6c7a9fcec773e4c3e9
Arlesson Moura <arlessonmss@gmail.com>
2021-03-22 01:03:07 -0300

    VqV function else emplements IV

--- src/vqv.js
+++ src/vqv.js
@@ -17,12 +17,14 @@
 */
 
 const vqv = (nome, idade) => {
-  if (typeof (nome) === 'string' && typeof (idade) === 'number') {
+  if (typeof (nome) !== 'string' && typeof (idade) !== 'number') {
+    return undefined;
+  } else {
     return `Oi, meu nome é ${nome}!
     Tenho ${idade} anos,
     trabalho na Trybe e mando muito em programação!
     #VQV!`;
-  } 
+  }
 };
 
 module.exports = vqv;
